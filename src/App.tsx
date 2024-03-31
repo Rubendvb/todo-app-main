80263f973a9861ede64019300731870d77bbb085
Rubendvb <rubenvasquez556@gmail.com>
2024-03-31 03:04:24 -0300

    functions clear and filter

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ function App() {
         setFilter={setFilter}
         filter={filter}
         itemQuantities={itemQuantities}
+        setItemQuantities={setItemQuantities}
       />
 
       <footer className="footer">
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import classNames from 'classnames'
 
 import { ITask } from '../../@types/tasks'
@@ -30,10 +30,32 @@ interface CardProps {
   setFilter: React.Dispatch<React.SetStateAction<Filter>>
   filter: Filter
   itemQuantities: number
+  setItemQuantities: React.Dispatch<React.SetStateAction<number>>
 }
 
-export default function Card({ itemQuantities, tasks, setTasks }: CardProps) {
+export default function Card({
+  itemQuantities,
+  tasks,
+  setTasks,
+  setItemQuantities,
+}: CardProps) {
   const [selected, setSelected] = useState('all')
+  const [tasksFiltered, setTasksFiltered] = useState<ITask[]>([])
+
+  const selectTaskStatus = (id: string) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          status: 'completed',
+        }
+      } else {
+        return task
+      }
+    })
+
+    setTasks(newTasks)
+  }
 
   const deletedTask = (id: string) => {
     const newTasks = tasks.filter((task) => task.id !== id)
@@ -41,14 +63,44 @@ export default function Card({ itemQuantities, tasks, setTasks }: CardProps) {
     setTasks(newTasks)
   }
 
+  const clearCompleted = () => {
+    const newTasks = tasks.filter((task) => task.status !== 'completed')
+
+    setTasks(newTasks)
+  }
+
+  useEffect(() => {
+    if (selected === 'active') {
+      const tasksActive = tasks.filter((task) => task.status !== 'completed')
+
+      setTasksFiltered(tasksActive)
+      setItemQuantities(tasksActive.length)
+    } else if (selected === 'completed') {
+      const tasksCompleted = tasks.filter((task) => task.status === 'completed')
+
+      setTasksFiltered(tasksCompleted)
+      setItemQuantities(tasksCompleted.length)
+    } else {
+      setTasksFiltered(tasks)
+      setItemQuantities(tasks.length)
+    }
+  }, [selected, tasks, setTasksFiltered, setItemQuantities])
+
   return (
     <main className="card">
       <ul>
         {tasks &&
-          tasks.map((task) => (
+          tasksFiltered.map((task) => (
             <li key={task.id}>
-              <label>
-                <input type="checkbox" name="checkbox" id={`${task.id}`} />
+              <label onChange={() => selectTaskStatus(task.id)}>
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  id={`${task.id}`}
+                  disabled={task.status === 'completed'}
+                  checked={task.status === 'completed'}
+                  onChange={() => selectTaskStatus(task.id)}
+                />
                 <span>{task.description}</span>
               </label>
 
@@ -77,7 +129,12 @@ export default function Card({ itemQuantities, tasks, setTasks }: CardProps) {
             </span>
           ))}
         </div>
-        <span className="card__footer__span clear">Clear Completed</span>
+        <span
+          className="card__footer__span clear"
+          onClick={() => clearCompleted()}
+        >
+          Clear Completed
+        </span>
       </div>
     </main>
   )
